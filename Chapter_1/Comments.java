72203057029bf814e621fee045d51c0a17c3c597
JeanneAmberPagusara-UCM <jeanneamber1207@gmail.com>
2025-09-02 16:58:41 +0800

    Update Comments.java

--- Chapter_1/Comments.java
+++ Chapter_1/Comments.java
@@ -1,1 +1,21 @@
-
+public class Comments {
+  public static void main(String[] args) {
+    
+    // Single-line comment
+    System.out.println("Single-line comment");
+        
+    /* 
+    This is a 
+    Multi-line 
+    comment
+    */
+    System.out.println("Multi-line Comment");
+        
+    /**
+    * Documentation
+    * comment
+    * @Pagusara
+    */
+    System.out.println("Documention Comment");
+  }
+}
