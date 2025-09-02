635c833d114be17bf84567a0c12328cc4e063ff4
JeanneAmberPagusara-UCM <jeanneamber1207@gmail.com>
2025-09-02 15:34:39 +0800

    Update FavoriteSong.java

--- Chapter_1/FavoriteSong.java
+++ Chapter_1/FavoriteSong.java
@@ -1,1 +1,8 @@
-
+public class FavoriteSong {
+  public static void main(String[] args) {
+        System.out.println("I'll be your cryin' shoulder");
+        System.out.println("I'll be love's suicide");
+        System.out.println("And I'll be better when I'm older");
+        System.out.println("I'll be the greatest fan of your life");
+    }
+  }
